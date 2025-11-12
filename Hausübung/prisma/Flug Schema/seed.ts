7d98f350caad487215fbd1707ee39eb2a044bdda
FlinttLogg77 <vir210341@spengergasse.at>
2025-11-12 13:33:10 +0100

    hmmm

--- Hausübung/prisma/Flug Schema/seed.ts
+++ Hausübung/prisma/Flug Schema/seed.ts
@@ -1,14 +1,26 @@
-import { PrismaClient } from "model";
+import { PrismaClient } from "./prisma/client/deno.ts";
 import { faker } from "@faker-js/faker";
+
 const prisma = new PrismaClient();
 
-for (let i = 0; i < 10; i++) {
-    const fake_airport = faker.airline.airport();
-    await prisma.airport.create({
-        data: {
-            name: fake_airport.name,
-            iataCode: fake_airport.iataCode,
-            city: faker.location.city(),
-        },
-    });
+async function main() {
+    for (let i = 0; i < 10; i++) {
+        const fakeAirport = faker.airline.airport();
+        await prisma.airport.create({
+            data: {
+                name: fakeAirport.name,
+                iataCode: fakeAirport.iataCode,
+                city: faker.location.city(),
+            },
+        });
+    }
 }
+
+main()
+    .catch((e) => {
+        console.error(e);
+        Deno.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
